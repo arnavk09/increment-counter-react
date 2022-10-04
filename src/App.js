1c9a01ce8f629db2fd84f9fe4094fddd52db30cb
arnavk09 <arnav02@outlook.com>
2022-10-04 19:25:23 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Navigation } from "./components/Navigation";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Navigation />
+        <h1 className="app">Demo App</h1>
+        <div className="buttonstuff">
+          <div className="incrementButton">
+            <Button variant="contained" startIcon={<AddIcon />} >
+              Incrementing
+            </Button>
+          </div>
+          <div className="decrement">
+            <Button variant="contained" startIcon={<RemoveIcon />}>
+              Decrementing
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
