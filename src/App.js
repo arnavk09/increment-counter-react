a4ebc00fcc2f02429bf181f6f7bb212d3d729b17
arnavk09 <arnav02@outlook.com>
2022-10-04 21:49:02 +0530

    added counter functionalities

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import "./App.css";
-import { Navigation } from "./components/Navigation";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import { useState } from "react";
 function App() {
+  const [count, setCount] = useState(0);
+  var incrementHandler = () => {
+    setCount(count + 1);
+    console.log("Increment button clicked. Count is " + count);
+  };
+  var decrementHandler = () => {
+    if (count <= 0) {
+      alert("Cannot decrement further. Count is already 0.");
+    } else {
+      setCount(count - 1);
+      console.log("Decrement button clicked. Count is " + count);
+    }
+  };
   return (
     <>
       <div>
-        <Navigation />
-        <h1 className="app">Demo App</h1>
+        {/* <Navigation /> */}
+        <h1 className="app">Counter App</h1>
         <div className="buttonstuff">
           <div className="incrementButton">
-            <Button variant="contained" startIcon={<AddIcon />} >
-              Incrementing
+            <Button
+              variant="contained"
+              startIcon={<AddIcon />}
+              onClick={incrementHandler}>
+              Increment
             </Button>
           </div>
           <div className="decrement">
-            <Button variant="contained" startIcon={<RemoveIcon />}>
-              Decrementing
+            <Button
+              variant="contained"
+              startIcon={<RemoveIcon />}
+              onClick={decrementHandler}>
+              Decrement
             </Button>
           </div>
         </div>
+        <h4 className="counter">Count:{count}</h4>
       </div>
     </>
   );
